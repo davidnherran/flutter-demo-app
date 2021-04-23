00d89010220152843b4ebf568113536ad663248d
davidnherran <davidnherran@gmail.com>
2021-04-23 14:41:23 -0500

    Create description section and star section

--- lib/description_place.dart
+++ lib/description_place.dart
@@ -1,9 +1,38 @@
 import 'package:flutter/material.dart';
 
 class DescriptionPlace extends StatelessWidget {
+
+  String namePlace;
+  int stars;
+  String descriptionPlace;
+
+  DescriptionPlace(this.namePlace, this.stars, this.descriptionPlace);
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
+    final start_empty = Container(
+      margin: EdgeInsets.only(
+        top: 323.0,
+        right: 3.0
+      ),
+      child: Icon(
+        Icons.star_border,
+        color: Color(0xFFf2c611),
+      ),
+    );
+
+    final star_half = Container(
+      margin: EdgeInsets.only(
+          top: 323.0,
+          right: 3.0
+      ),
+      child: Icon(
+        Icons.star_half,
+        color: Color(0xFFf2c611),
+      ),
+    );
 
     final star = Container(
       margin: EdgeInsets.only(
@@ -16,6 +45,22 @@ class DescriptionPlace extends StatelessWidget {
       ),
     );
 
+    final description = Container(
+      margin: new EdgeInsets.only(
+        top: 20.0,
+        left: 20.0,
+        right: 20.0
+      ),
+      child: new Text(
+        descriptionPlace,
+        style: const TextStyle(
+          fontSize: 16.0,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFF56575a)
+        ),
+      ),
+    );
+
     final title_stars = Row(
       children: <Widget>[
         Container(
@@ -25,7 +70,7 @@ class DescriptionPlace extends StatelessWidget {
             right: 20.0
           ),
           child: Text(
-            "Duwili Ella",
+            namePlace,
             style: TextStyle(
               fontSize: 30.0,
               fontWeight: FontWeight.w900
@@ -38,14 +83,20 @@ class DescriptionPlace extends StatelessWidget {
             star,
             star,
             star,
-            star,
-            star
+            star_half,
+            start_empty
           ],
-        )
+        ),
+
       ],
     );
 
-    return title_stars;
+    return Column(
+      children: [
+        title_stars,
+        description
+      ],
+    );
   }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
+  String descriptionDummy = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -27,7 +29,7 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text("Hola Mundo")
         ),
-        body: new DescriptionPlace(),
+        body: new DescriptionPlace("Bahamas", 4, descriptionDummy),
       )
       //MyHomePage(title: 'Flutter Demo Home Page'),
     );
